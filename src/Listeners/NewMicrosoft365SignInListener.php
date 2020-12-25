d82e2376dea53f437fdbc2635471e4556627bce4
David Carr <dave@dcblog.dev>
2020-12-25 00:27:21 +0000

    added event to fire on sign in to allow listeners to run on click applications

---
+++ src/Listeners/NewMicrosoft365SignInListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Dcblogdev\MsGraph\Models\MsGraphToken;
+use Illuminate\Support\Facades\Auth;
+
+class NewMicrosoft365SignInListener
+{
+    public function handle($event)
+    {
+        $tokenId = $event->token['token_id'];
+        $token = MsGraphToken::find($tokenId)->first();
+
+        if ($token->user_id == null) {
+            $user = User::create([
+                'name'  => $event->token['info']['displayName'],
+                'email' => $event->token['info']['mail'],
+                'password' => ''
+            ]);
+
+            $token->user_id = $user->id;
+            $token->save();
+
+            Auth::login($user);
+
+        } else {
+            $user = User::findOrFail($token->user_id);
+            $user->save();
+
+            Auth::login($user);
+        }
+    }
+}
