f0986ddb40ce725188cea33f1f0b5c1367579223
thursdaydan <d.sherwood@theonepoint.co.uk>
2021-08-24 08:33:16 +0100

    Create Events.php

---
+++ src/AdminResources/Events.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Dcblogdev\MsGraph\AdminResources;
+
+use Dcblogdev\MsGraph\Facades\MsGraphAdmin;
+
+class Events extends MsGraphAdmin
+{
+    private $userId;
+    private $top;
+    private $skip;
+
+    public function userid($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    public function top($top)
+    {
+        $this->top = $top;
+        return $this;
+    }
+
+    public function skip($skip)
+    {
+        $this->skip = $skip;
+        return $this;
+    }
+
+    public function get($params = [])
+    {
+        if ($this->userId == null) {
+            throw new Exception("userId is required.");
+        }
+
+        $top = request('top', $this->top);
+        $skip = request('skip', $this->skip);
+
+        if ($params == []) {
+            $params = http_build_query([
+                "\$orderby" => "subject",
+                "\$top" => $top,
+                "\$skip" => $skip,
+                "\$count" => "true",
+            ]);
+        } else {
+            $params = http_build_query($params);
+        }
+
+        $events = MsGraphAdmin::get("users/$this->userId/events?$params");
+
+        $data = MsGraphAdmin::getPagination($events, $top, $skip);
+
+        return [
+            'events' => $events,
+            'total' => $data['total'],
+            'top' => $data['top'],
+            'skip' => $data['skip'],
+        ];
+    }
+
+    public function find($id)
+    {
+        if ($this->userId == null) {
+            throw new Exception("userId is required.");
+        }
+
+        return MsGraphAdmin::get("users/$this->userId/events/$id");
+    }
+
+    public function store(array $data)
+    {
+        if ($this->userId == null) {
+            throw new Exception("userId is required.");
+        }
+
+        return MsGraphAdmin::post("users/$this->userId/events", $data);
+    }
+
+    public function update($id, array $data)
+    {
+        if ($this->userId == null) {
+            throw new Exception("userId is required.");
+        }
+
+        return MsGraphAdmin::patch("users/$this->userId/events/$id", $data);
+    }
+
+    public function delete($id)
+    {
+        if ($this->userId == null) {
+            throw new Exception("userId is required.");
+        }
+
+        return MsGraphAdmin::delete("users/$this->userId/events/$id");
+    }
+}
