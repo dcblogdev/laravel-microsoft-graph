8270f302e2128a56f3c2ea576e436ef0a59d44b0
David Carr <dave@dcblog.dev>
2022-08-28 11:17:58 +0100

    updated test foundation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Dcblogdev\MsGraph\Tests;
 
+use create_ms_graph_tokens_table;
 use Dcblogdev\MsGraph\MsGraphServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -12,5 +13,27 @@ class TestCase extends Orchestra
         return [
             MsGraphServiceProvider::class,
         ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'default');
+        $app['config']->set('database.connections.default', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        require_once 'src/database/migrations/create_ms_graph_tokens_table.php';
+
+        // run the up() method of that migration class
+        (new create_ms_graph_tokens_table)->up();
     }
 }
