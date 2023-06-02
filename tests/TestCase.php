e056c8010e322e31d8e576ac99b5a96d8903e350
David Carr <dave@dcblog.dev>
2023-06-02 09:01:20 +0100

    removed unused imports from testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,15 +3,10 @@
 namespace Dcblogdev\MsGraph\Tests;
 
 use Dcblogdev\MsGraph\MsGraphServiceProvider;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as Orchestra;
-use function Orchestra\Testbench\artisan;
 
 class TestCase extends Orchestra
 {
-    //use DatabaseMigrations;
-
     protected function getPackageProviders($app)
     {
         return [
@@ -35,12 +30,6 @@ class TestCase extends Orchestra
         $this->loadLaravelMigrations();
 
         $this->loadMigrationsFrom(dirname(__DIR__).'/database/migrations');
-
-//        artisan($this, 'migrate');
-//
-//        $this->beforeApplicationDestroyed(
-//            fn () => artisan($this, 'migrate:rollback')
-//        );
     }
 }
 
