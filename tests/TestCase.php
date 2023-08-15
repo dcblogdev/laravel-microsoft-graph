606fc43daf818d9fd8ec90a7be5d27cf5a334c2b
David Carr <dave@dcblog.dev>
2023-08-15 12:04:52 +0100

    load migrations from src directory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -29,7 +29,7 @@ class TestCase extends Orchestra
     {
         $this->loadLaravelMigrations();
 
-        $this->loadMigrationsFrom(dirname(__DIR__).'/database/migrations');
+        $this->loadMigrationsFrom(dirname(__DIR__).'/src/database/migrations');
     }
 }
 
