87d68c5fa270f2f7ed6cb39956544a98119f1b96
David Carr <dave@dcblog.dev>
2023-11-04 10:35:34 +0000

    removed bin

--- bin/git-hooks/post-checkout.d/composer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CHANGED=`git diff $1 $2 --stat -- ./composer.lock | wc -l`
-if [ $CHANGED -gt 0 ];
-then
-    printf "\033[33m⚠ Warning: composer.lock has changed. Update now with composer install. ⚠\033[0m\n"
-fi
