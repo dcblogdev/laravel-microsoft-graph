e7d099757278ec708b18705acfd9ea9032f2736c
Markus Diehl <mail@mrksdiehl.com>
2024-05-03 14:39:08 +0200

    Fix: Add string as argument type to isJson

--- src/MsGraphAdmin.php
+++ src/MsGraphAdmin.php
@@ -162,7 +162,7 @@ class MsGraphAdmin
         }
     }
 
-    protected function isJson(object $data): bool
+    protected function isJson(object|string $data): bool
     {
         return is_string($data) && is_array(json_decode($data, true)) && (json_last_error() == JSON_ERROR_NONE);
     }
