a8e2d58d11ff7e3f3a8af4d8a96146d52ea7c467
David Carr <dave@dcblog.dev>
2024-11-14 02:33:14 +0000

    corrected return types

--- src/Resources/Tasks/TaskLists.php
+++ src/Resources/Tasks/TaskLists.php
@@ -6,33 +6,33 @@ use Dcblogdev\MsGraph\Facades\MsGraph;
 
 class TaskLists extends MsGraph
 {
-    public function get(array $params = []): MsGraph
+    public function get(array $params = []): array
     {
         $params = http_build_query($params);
 
         return MsGraph::get('me/todo/lists?'.$params);
     }
 
-    public function find(string $listId): MsGraph
+    public function find(string $listId): array
     {
         return MsGraph::get("me/todo/lists/$listId");
     }
 
-    public function store(string $name): MsGraph
+    public function store(string $name): array
     {
         return MsGraph::post('me/todo/lists', [
             'displayName' => $name,
         ]);
     }
 
-    public function update(string $listId, string $name): MsGraph
+    public function update(string $listId, string $name): array
     {
         return MsGraph::patch("me/todo/lists/$listId", [
             'displayName' => $name,
         ]);
     }
 
-    public function delete(string $listId): MsGraph
+    public function delete(string $listId): string
     {
         return MsGraph::delete("me/todo/lists/$listId");
     }
